9d97c295071d2e3e2462045fc3ed348f5e834969
Manpreet Singh <mannyy91s@gmail.com>
2016-11-29 13:18:14 -0500

    converted all html pages into react components

---
+++ webpack/components/SignIn.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+class SignIn extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div className="container-fluid signin-container">
+             <div className="row">
+               <h1 className="text-center">Grain of Salt</h1>
+                <div className="col-sm-6 col-sm-offset-3 text-center fb-button">
+                  <button className="btn btn-primary">Signin with Facebook</button>
+                  <h4>or</h4>
+                  <hr />
+                </div>
+                <div className="col-sm-6 col-sm-offset-3">
+                  <div className="form-group">
+                    <label>Email</label>
+                    <input id="signInEmail" type="text" name="email" className="form-control" required/>
+                  </div>
+                  <div className="form-group">
+                      <label>Password</label>
+                      <input id="signInPassword" type="password" name="password" className="form-control" required/>
+                  </div>
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <a href="#">Forget Password?</a>
+                    </div>
+                    <div className="col-sm-6 text-right">
+                      <button id="login" type="button" className="btn btn-success">Login</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        )
+    }
+}
+export default SignIn
