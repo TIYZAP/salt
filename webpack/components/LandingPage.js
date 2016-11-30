f182929f29d2e29491e7064ec7d148bed007624b
Manpreet Singh <mannyy91s@gmail.com>
2016-11-30 14:26:07 -0500

    made more styling changes to pages

--- webpack/components/LandingPage.js
+++ webpack/components/LandingPage.js
@@ -9,15 +9,17 @@ class LandingPage extends React.Component {
         return(
             <div className="container-fluid landing-container">
                     <div className="row">
-                        <div className="col-sm-12 text-center">
-                            <h1>Grain of Salt</h1>
-                            <h3>Reviews by your trusted network</h3>
-                        </div>
-                        <div className="col-sm-2 col-sm-offset-5">
-                            <Link to="/signin"><button id="signin" type="button" className="btn btn-primary btn-block">SignIn</button></Link>
-                        </div>
-                        <div className="col-sm-2 col-sm-offset-5">
-                            <Link to="/signup"><button id="signup" type="button" className="btn btn-success btn-block">SignUp</button></Link>
+                        <div className="col-sm-6 col-sm-offset-3 landingpage-box">
+                            <div className="col-sm-12 text-center">
+                                <h1>Grain of Salt</h1>
+                                <h3>Reviews by your trusted network</h3>
+                            </div>
+                            <div className="col-sm-3 col-sm-offset-2">
+                                <Link to="/signin"><button id="signin" type="button" className="btn btn-primary btn-block">SignIn</button></Link>
+                            </div>
+                            <div className="col-sm-3 col-sm-offset-2">
+                                <Link to="/signup"><button id="signup" type="button" className="btn btn-success btn-block">SignUp</button></Link>
+                            </div>
                         </div>
                 </div>
             </div>
