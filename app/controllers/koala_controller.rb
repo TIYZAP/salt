4a201208ac5e0227a8b244f63684c70b1200dd39
jkrouse13 <jon.krouse@gmail.com>
2016-11-30 13:55:14 -0500

    changed follow facebook method to find by facebook id instead of name

--- app/controllers/koala_controller.rb
+++ app/controllers/koala_controller.rb
@@ -1,5 +1,19 @@
 class KoalaController < ApplicationController
 
+  def follow_facebook
+    @user = current_user
+    @graph = Koala::Facebook::API.new(@user.fb_token)
+    friends = @graph.get_connections("me", "friends")
+    friends.each do |f|
+      @thing = User.find_by(fb_id: f['id'])
+      puts f['name'].inspect
+      puts f.inspect
+      @user.follow!(@thing)
+    end
+    @followees = @user.followees(User)
+    render json: @followees
+  end
+
   def test
     @user = current_user
     @graph = Koala::Facebook::API.new(@user.fb_token)
