8522475925d174193c1164474b809a891b02a816
Manpreet Singh <mannyy91s@gmail.com>
2016-12-01 08:48:26 -0500

    testing facebook friends on our app

--- webpack/components/Home.js
+++ webpack/components/Home.js
@@ -10,32 +10,36 @@ class Home extends React.Component {
         return(
             <div>
                 <Menu />
-            <div className="container-fluid main-body-home">
                 <div className="nav-bar-left">
                     <div>
-                        <img className="img-thumbnail" src="http://unsplash.it/600/600?random" alt="" />
+                        <img className="img-rounded" src="http://unsplash.it/600/600?random" alt="" />
                     </div>
                     <ul>
-                        <Link to="/" style={{textDecoration: 'none'}}><li>Home</li></Link>
-                        <Link to="/friends" style={{textDecoration: 'none'}}><li>Friends</li></Link>
+                        <Link to="/" style={{textDecoration: 'none'}}><li><i className="fa fa-home" aria-hidden="true"> Home</i>
+                        </li></Link>
+                        <Link to="/friends" style={{textDecoration: 'none'}}><li><i className="fa fa-users" aria-hidden="true"> Friends</i>
+                        </li></Link>
                         <Link to="/review" style={{textDecoration: 'none'}}><li>Review</li></Link>
-                        <Link to="/search" style={{textDecoration: 'none'}}><li>Search</li></Link>
+                        <Link to="/search" style={{textDecoration: 'none'}}><li><i className="fa fa-search" aria-hidden="true"> Search</i>
+                        </li></Link>
                         <Link to="/friendprofile" style={{textDecoration: 'none'}}><li>FriendProfile</li></Link>
                         <Link to="/landingpage" style={{textDecoration: 'none'}}><li>LandingPage</li></Link>
                         <Link to="/signin" style={{textDecoration: 'none'}}><li>Signin</li></Link>
-                        <Link to="/signup" style={{textDecoration: 'none'}}><li>Signup</li></Link>
-                        <li>Logout</li>
+                        <Link to="/signup" style={{textDecoration: 'none'}}><li>Signup
+                        </li></Link>
+                        <Link tp="/landingpage"><li><i className="fa fa-sign-out" aria-hidden="true"> Logout</i></li></Link>
                     </ul>
                 </div>
+            <div className="container-fluid main-body-home">
                 <div className="row">
-                    <div className="col-sm-9 col-sm-offset-3 main-body-right">
-                        <div className="text-center logo">
+                    <div className="col-sm-12 col-sm-offset-3 main-body-right">
+                        <div className="col-sm-9 text-center logo">
                             <h1>Grain of Salt</h1>
                         </div>
                         <div className="col-sm-12">
                             <div className="col-sm-8 col-sm-offset-2 reviews">
                                 <div className="col-sm-4">
-                                    <img className="img-thumbnail" src="http://unsplash.it/400/300?random" alt="" />
+                                    <img className="img-rounded" src="http://unsplash.it/400/300?random" alt="" />
                                     <h1>Manny</h1>
                                 </div>
                                 <div className="col-sm-8">
@@ -46,7 +50,7 @@ class Home extends React.Component {
                             </div>
                             <div className="col-sm-8 col-sm-offset-2 reviews">
                                 <div className="col-sm-4">
-                                    <img className="img-thumbnail" src="http://unsplash.it/400/300?random" alt="" />
+                                    <img className="img-rounded" src="http://unsplash.it/400/300?random" alt="" />
                                     <h1>Manny</h1>
                                 </div>
                                 <div className="col-sm-8">
@@ -57,7 +61,7 @@ class Home extends React.Component {
                             </div>
                             <div className="col-sm-8 col-sm-offset-2 reviews">
                                 <div className="col-sm-4">
-                                    <img className="img-thumbnail" src="http://unsplash.it/400/300?random" alt="" />
+                                    <img className="img-rounded" src="http://unsplash.it/400/300?random" alt="" />
                                     <h1>Manny</h1>
                                 </div>
                                 <div className="col-sm-8">
@@ -68,7 +72,7 @@ class Home extends React.Component {
                             </div>
                             <div className="col-sm-8 col-sm-offset-2 reviews">
                                 <div className="col-sm-4">
-                                    <img className="img-thumbnail" src="http://unsplash.it/400/300?random" alt="" />
+                                    <img className="img-rounded" src="http://unsplash.it/400/300?random" alt="" />
                                     <h1>Manny</h1>
                                 </div>
                                 <div className="col-sm-8">
