340e3626586d1e6026035f6a9f507b05d8894d28
jkrouse13 <jon.krouse@gmail.com>
2016-12-02 10:28:36 -0500

    Added category and name search functionality.

--- app/controllers/geo_search_controller.rb
+++ app/controllers/geo_search_controller.rb
@@ -1,19 +1,17 @@
 class GeoSearchController < ApplicationController
   include Geokit::Geocoders
-  # def test
-  #   @client = GooglePlaces::Client.new(ENV['GOOGLE_API'])
-  #   @lat =
-  #
-  #   render json: @client.spots(39.7682937, -86.1516103, :types => 'restaurant', :radius => 8046)
-  #
-  # end
 
   def search
     res = MultiGeocoder.geocode(params[:address])
     @lat = res.lat
     @lng = res.lng
     @client = GooglePlaces::Client.new(ENV['GOOGLE_API'])
-    render json: @client.spots(@lat, @lng, :types => 'restaurant', :radius => 8046).map{|this_one| build_hash_for_output(this_one) }
+    if params[:name].present?
+      @name = params[:name]
+      render json: @client.spots(@lat, @lng, :name => @name, :types => 'restaurant',:radius => 8046).map{|this_one| build_hash_for_output(this_one) }
+    else
+      render json: @client.spots(@lat, @lng, :types => 'restaurant', :radius => 8046).map{|this_one| build_hash_for_output(this_one) }
+    end
   end
 
   def show
