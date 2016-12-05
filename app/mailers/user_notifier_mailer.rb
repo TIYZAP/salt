4f267c1190c214ed78333245cf8c7991b4dd0619
jkrouse13 <jon.krouse@gmail.com>
2016-12-05 10:56:49 -0500

    changed what the new mailer decends from to an action:mailer

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,10 +1,10 @@
-class UserNotifierMailer < ApplicationMailer
-  default :from => 'Admin@salty-app.com'
+class UserNotifier < ActionMailer::Base
+  default :from => 'any_from_address@example.com'
 
- # send a signup email to the user, pass in the user object that   contains the user's email address
- def send_signup_email(user)
-   @user = user
-   mail( :to => @user.email,
-   :subject => 'Thanks for signing up for our amazing app' )
- end
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
 end
