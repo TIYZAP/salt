025aa51ff782e53947388116b2fb27635534b4ca
Manpreet Singh <mannyy91s@gmail.com>
2016-12-08 15:56:58 -0500

    made a bunch of design changes to landing page, home page, and other things.

--- webpack/components/LandingPage.js
+++ webpack/components/LandingPage.js
@@ -14,8 +14,8 @@ class LandingPage extends React.Component {
                                 <h1>Grain of Salt</h1>
                                 <h3>Reviews by your trusted network</h3>
                             </div>
-                            <div className="col-sm-3 col-sm-offset-4">
-                            <a className="btn btn-primary" href="/users/auth/facebook"><i className="fa fa-facebook-official fa-lg" aria-hidden="true"></i> Signin with Facebook</a>
+                            <div className="col-sm-4 col-sm-offset-3">
+                            <a className="btn btn-primary" href="/users/auth/facebook"><i className="fa fa-facebook-official" aria-hidden="true"></i> Signin with Facebook</a>
                             </div>
                         </div>
                 </div>
