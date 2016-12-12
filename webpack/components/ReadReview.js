ca719a4d3b14945d4497cf5ba051f399e522250b
Manpreet Singh <mannyy91s@gmail.com>
2016-12-12 16:58:43 -0500

    made design changes for reading a review and reviews posted below

--- webpack/components/ReadReview.js
+++ webpack/components/ReadReview.js
@@ -20,27 +20,42 @@ class ReadReview extends React.Component{
         fetch('/api/friends/reviews?place_id='+ window.location.href.split('?')[1].replace('place_id=','') + '&user_email=' + sessionStorage.getItem('email') + '&user_token=' + sessionStorage.getItem('token'))
         .then(response => response.json())
         .then(response => this.setState({reviews: response.reviews}))
+        // .then(response => {
+        //     console.log(response)
+        // })
     }
     render(){
         console.log(this.state.reviews)
         if(this.state.reviews){
             var friendsReviews = this.state.reviews.map((review, i) => {
-               return               <div className="col-sm-12 home-middle-middle-review" key={i}>
-                                       <div className="col-sm-4">
-                                         <h3 className="text-center">{review.user.name}</h3>
-                                           <img className="img-rounded" src={review.user.image} alt="Reviewers Picture" />
-                                           <p className="text-center">{moment(review.created_at).fromNow()}</p>
+               return             <div key={i}>
+                                   <div className="row home-middle-middle-readreview-place">
+                                       <div className="col-sm-6">
+                                           <img height="300" className="img-rounded" src={review.image} alt="Place Picture" />
                                        </div>
-                                       <div className="col-sm-8">
-                                           <h1 className="text-center">{review.venue_name}</h1>
-                                           <p>Dish: {review.dish}</p>
-                                           Rating: <ReactStars count={review.rating} edit={false} color1={'#Eb8a3e'} />
+                                       <div className="col-sm-6">
+                                           <h4>{review.venue_name}</h4>
                                            <p>Address: {review.venue_address}</p>
                                            <p>Phone: {review.phone}</p>
                                            <p>Website: <a href={review.website}>Link to website</a></p>
-                                           <h4>Review: <br />{review.body}</h4>
                                        </div>
                                    </div>
+                                    <div className="col-sm-12 home-middle-middle-readreview-inner" >
+                                        <div className="row">
+                                            <div className="col-sm-2 readreview-img">
+                                              <p>{review.user.name}</p>
+                                                <img className="img-circle" src={review.user.image} alt="Reviewers Picture" />
+                                                <p>{moment(review.created_at).fromNow()}</p>
+                                            </div>
+                                            <div className="col-sm-10">
+                                                <ReactStars count={review.rating} edit={false} color1={'#Eb8a3e'} size={18}/>
+                                                <br />
+                                                <p>Dish: {review.dish}</p>
+                                                <p>Review: <br />{review.body}</p>
+                                            </div>
+                                        </div>
+                                   </div>
+                                  </div>
            })}
            else{
                var friendsReviews = <div className="home-middle-middle-review">
