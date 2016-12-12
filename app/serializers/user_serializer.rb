6768267b472fabbaeef0a57432cdce585e5af4d8
jkrouse13 <jon.krouse@gmail.com>
2016-12-12 16:29:26 -0500

    serialized badges for users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :name, :image, :email, :id, :review_count
   has_many :reviews
+  has_many :badges
 
   def image
     Refile.attachment_url(object, :image, :fit, 200, 200, format: "jpg")
