2721ecd4ba8c1ede3a0c907878e1b5ac763ce83e
jkrouse13 <jon.krouse@gmail.com>
2016-12-13 11:27:16 -0500

    starting to clean up commented out and unused code.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,7 +19,7 @@ before_action :configure_sign_in_params, only: [:create]
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
